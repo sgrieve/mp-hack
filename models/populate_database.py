9c397318bbcb1a00fa909573637931035029b819
Scott Archer-Nicholls <s.archer-nicholls@gmail.com>
2024-05-02 14:19:08 +0100

    Added relationship matching function

--- models/populate_database.py
+++ models/populate_database.py
@@ -20,8 +20,9 @@ def add_mp(dict_in, df):
 
 def add_subject(dict_in, df):
     """Add an entry to dataframe of subject.csv
+        TODO
     """
-
+    return df
 
 def add_university(dict_in, df):
     """Add an entry to dataframe of university.csv
@@ -50,7 +51,33 @@ def add_university(dict_in, df):
         return pd.concat([df, row], ignore_index=True)
 
 
-def add_relationship(dict_in, df_mp, df_sub, df_uni, df_rel):
+def add_relationship(dict_in, df_mp, df_uni, df_rel, df_sub=None):
     """Add an entry to dataframe of relationship.csv,
        linking indexes of the different database csv files
+
+       TODO: Add logic for subject once it is ready
     """
+    # Find MP name and university in DF
+    # print(df_mp["Name"])
+    if dict_in["Name"] in list(df_mp["Name"]):
+        mp_id = df_mp.loc[df_mp['Name'] == dict_in["Name"]]["ID"]
+    else:
+        raise ValueError(f"{dict_in['Name']} not in MP dataframe")
+
+    uni_ids = []
+    for uni in dict_in["Education"]:
+        if uni["UniName"] in list(df_uni["UniName"]):
+            uni_ids.append(df_uni.loc[df_uni['UniName'] == uni["UniName"]]["ID"])
+        else:
+            raise ValueError(f"{uni['UniName']} not in df_uni")
+
+    # Add rows for each uni / subject
+    rows = pd.DataFrame(
+        [{"MP": int(mp_id),
+        "University": int(uni_id),
+        "Subject": np.nan} for uni_id in uni_ids]
+    )
+
+    # TODO: Add a check to make sure rows do not currently exist
+    return pd.concat([df_rel, rows], ignore_index=False)
+
