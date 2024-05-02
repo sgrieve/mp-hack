adace88e1aa3ee9788856715f35c7ab4294b6b3e
tobfer <soutobias@yahoo.com.br>
2024-05-02 13:31:43 +0100

    create endpoints

--- app.py
+++ app.py
@@ -20,3 +20,26 @@ def test():
 def mp():
     data = pd.read_csv('database/mp.csv')
     return data.to_dict(orient='records')
+
+@app.get("/subject")
+def subject():
+    data = pd.read_csv('database/subject.csv')
+    return data.to_dict(orient='records')
+
+@app.get("/university")
+def university():
+    data = pd.read_csv('database/university.csv')
+    return data.to_dict(orient='records')
+
+@app.get("/relationship")
+def relationship():
+    mp = pd.read_csv('database/mp.csv')
+    subject = pd.read_csv('database/subject.csv')
+    university = pd.read_csv('database/university.csv')
+    relationship = pd.read_csv('database/relationship.csv')
+
+    merged_df = pd.merge(relationship, mp, left_on='MP', right_on='ID', how='left')
+    merged_df = pd.merge(merged_df, subject, left_on='Subject', right_on='ID', how='left')
+    merged_df = pd.merge(merged_df, university, left_on='University', right_on='ID', how='left')
+    merged_df = merged_df.replace([float('inf'), float('-inf'), float('NaN')], None)
+    return merged_df.to_dict(orient='records')
